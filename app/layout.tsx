321a3e060dc66e9e18ac151473483718b147205e
Kartik <kkartiktummawar@gmail.com>
2025-02-08 09:36:13 +0530

    BUG FIX

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,11 +4,6 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Footer } from "@/components/ui/large-name-footer";
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "sharemyjson.",
